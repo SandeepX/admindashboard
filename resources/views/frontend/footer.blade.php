a3e9e83e3af22208f6b165b15def1d72107505bf
sandeeppant <sandeeppant024@gmail.com>
2020-08-07 12:42:45 +0545

    frontend added

---
+++ resources/views/frontend/footer.blade.php
@@ -0,0 +1,74 @@
+ <footer class="footer">
+        <div class="footer_top">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-xl-6 col-md-6">
+                        <div class="menu_links">
+                            <ul>
+                                <li><a href="#">About</a></li>
+                                <li><a href="#">Services</a></li>
+                                <li><a href="#">Portfolio</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-md-6">
+                        <div class="socail_links">
+                            <ul>
+                                <li><a href="#"> <i class="fa fa-facebook"></i> </a></li>
+                                <li><a href="#"> <i class="fa fa-twitter"></i> </a></li>
+                                <li><a href="#"> <i class="fa fa-instagram"></i> </a></li>
+                                <li><a href="#"> <i class="fa fa-google-plus"></i> </a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="copy-right_text">
+            <div class="container">
+                <div class="footer_border"></div>
+                <div class="row">
+                    <div class="col-xl-12">
+                        <p class="copy_right text-center">
+                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by sandeep</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--/ footer end  -->
+
+    <!-- JS here -->
+    <script src="{{asset('sandeep/js/vendor/modernizr-3.5.0.min.js')}}"></script>
+    <script src="{{asset('sandeep/js/vendor/jquery-1.12.4.min.js')}}"></script>
+    <script src="{{asset('sandeep/js/popper.min.js')}}"></script>
+    <script src="{{asset('sandeep/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('sandeep/js/owl.carousel.min.js')}}"></script>
+    <script src="{{asset('sandeep/js/isotope.pkgd.min.js')}}"></script>
+    <script src="{{asset('sandeep/js/ajax-form.js')}}"></script>
+    <script src="{{asset('sandeep/js/waypoints.min.js')}}"></script>
+    <script src="{{asset('sandeep/js/jquery.counterup.min.js')}}"></script>
+    <script src="{{asset('sandeep/js/imagesloaded.pkgd.min.js')}}"></script>
+    <script src="{{asset('sandeep/js/scrollIt.js')}}"></script>
+    <script src="{{asset('sandeep/js/jquery.scrollUp.min.js')}}"></script>
+    <script src="{{asset('sandeep/js/wow.min.js')}}"></script>
+    <script src="{{asset('sandeep/js/nice-select.min.js')}}"></script>
+    <script src="{{asset('sandeep/js/jquery.slicknav.min.js')}}"></script>
+    <script src="{{asset('sandeep/js/jquery.magnific-popup.min.js')}}"></script>
+    <script src="{{asset('sandeep/js/plugins.js')}}"></script>
+    <script src="{{asset('sandeep/js/gijgo.min.js')}}"></script>
+
+    <!--contact js-->
+    <script src="{{asset('sandeep/js/contact.js')}}"></script>
+    <script src="{{asset('sandeep/js/jquery.ajaxchimp.min.js')}}"></script>
+    <script src="{{asset('sandeep/js/jquery.form.js')}}"></script>
+    <script src="{{asset('sandeep/js/jquery.validate.min.js')}}"></script>
+    <script src="{{asset('sandeep/js/mail-script.js')}}"></script>
+
+    <script src="{{asset('sandeep/js/main.js')}}"></script>
+</body>
+
+</html>
---
+++ resources/views/frontend/header.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Phenomenal Sandeep</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- <link rel="manifest" href="site.webmanifest"> -->
+    <!-- Place favicon.ico in the root directory -->
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="{{asset('sandeep/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('sandeep/css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{asset('sandeep/css/magnific-popup.css')}}">
+    <link rel="stylesheet" href="{{asset('sandeep/css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('sandeep/css/themify-icons.css')}}">
+    <link rel="stylesheet" href="{{asset('sandeep/css/nice-select.css')}}">
+    <link rel="stylesheet" href="{{asset('sandeep/css/flaticon.css')}}">
+    <link rel="stylesheet" href="{{asset('sandeep/css/gijgo.css')}}">
+    <link rel="stylesheet" href="{{asset('sandeep/css/animate.min.css')}}">
+    <link rel="stylesheet" href="{{asset('sandeep/css/slick.css')}}">
+    <link rel="stylesheet" href="{{asset('sandeep/css/slicknav.css')}}">
+    <link rel="stylesheet" href="{{asset('sandeep/css/style.css')}}">
+    <!-- <link rel="stylesheet" href="css/responsive.css"> -->
+</head>
